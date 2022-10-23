1fda3ecfd5807058033ded93e5a14bb062a0ad5e
Zuzana Borutova <z.kasikova@gmail.com>
2022-10-23 09:04:34 +0200

    ještě nefunkční cena

--- src/components/Topping/index.jsx
+++ src/components/Topping/index.jsx
@@ -1,11 +1,17 @@
-import React, { useState } from "react";
-import Check from "../Check";
+import React, { useState } from 'react';
+import Check from '../Check';
+import ToppingsSelect from '../ToppingsSelect';
 import './style.css';
 
-const Topping = ({ topping }) => {
+const Topping = ({ topping, onChange }) => {
+  // const [checked, setChecked] = useState(false);
+  // const handleChange = (newChecked) => {
+  //   setChecked(newChecked);
+  // };
+
   return (
     <div className="topping">
-      <Check />
+      <Check checked={topping.selected} onChange={onChange} />
       <span className="topping__content">
         {topping.name}: {topping.price} €
       </span>
--- src/components/ToppingsSelect/index.jsx
+++ src/components/ToppingsSelect/index.jsx
@@ -1,16 +1,36 @@
-import React, { useState } from "react";
-import Topping from "../Topping";
+import React, { useState } from 'react';
+import Topping from '../Topping';
 import './style.css';
 
 const ToppingsSelect = ({ toppings }) => {
+  const [select, setSelect] = useState(toppings);
+
+  const handleChange = (index, newChecked) => {
+    const newThings = [...select];
+    newThings[index].selected = newChecked;
+    setSelect(newThings);
+  };
+
+  let count = 0;
+  select.forEach((topping) => (count += topping.selected));
+
+  // let sum = 0;
+  // select.forEach((topping) => (sum += topping.price));
+
   return (
     <>
       <p>Choose as many toppings as you want</p>
-      <p>Selected toppings: 0, total price: 0 Euro</p>
-        
+      <p>
+        Selected toppings: {count}, total price: {sum} Euro
+      </p>
+
       <div class="toppings">
-        {toppings.map((topping) => (
-          <Topping topping={topping} key={topping.name} />
+        {toppings.map((topping, index) => (
+          <Topping
+            topping={topping}
+            key={topping.name}
+            onChange={(newChecked) => handleChange(index, newChecked)}
+          />
         ))}
       </div>
     </>
